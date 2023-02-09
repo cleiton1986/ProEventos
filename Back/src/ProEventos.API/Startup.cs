b103625231f385b957288bfe8c7230e41f517b35
cleiton1986 <36868612+cleiton1986@users.noreply.github.com>
2023-02-09 11:08:01 -0300

    angular e plugins

--- Back/src/ProEventos.API/Startup.cs
+++ Back/src/ProEventos.API/Startup.cs
@@ -33,6 +33,8 @@ namespace ProEventos.API
                 context => context.UseSqlite(Configuration.GetConnectionString("Default"))
             );
             services.AddControllers();
+            services.AddCors();
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "ProEventos.API", Version = "v1" });
@@ -50,10 +52,13 @@ namespace ProEventos.API
             }
 
             app.UseHttpsRedirection();
-
             app.UseRouting();
-
             app.UseAuthorization();
+         
+            app.UseCors(x => x.AllowAnyHeader()
+                             .AllowAnyMethod()
+                             .AllowAnyOrigin());
+
 
             app.UseEndpoints(endpoints =>
             {
